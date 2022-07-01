a9c336613d4adaec627c8cb4039fb1203d1fa05e
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-07-01 23:04:40 +0900

    🌩️ 템플릿 코드 생성

---
+++ src/main/java/com/example/weneedtocharge/WeNeedToChargeApplication.java
@@ -0,0 +1,13 @@
+package com.example.weneedtocharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeNeedToChargeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeNeedToChargeApplication.class, args);
+    }
+
+}
